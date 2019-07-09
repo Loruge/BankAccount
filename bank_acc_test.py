99e63422d7c786bb4c024254a72a9228594134b5
testers <testers@itstep.kharkov.ua>
2019-07-09 20:45:35 +0300

    zeze

--- bank_acc_test.py
+++ bank_acc_test.py
@@ -25,6 +25,19 @@ class TestBankAccount(unittest.TestCase):
         incorrect_attempt = self.terminal.enter_pin_code(546)
         self.assertNotEqual(self.terminal.attempts, 3)
 
-    def test_user_can_withdraw_money_with_correct_pin(self):
-        correct_pin_attempt = self.terminal.enter_pin_code(333)
+    def test_attempts_0_after_3_incorrect_pins(self):
+        incorrect_at_1 = self.terminal.enter_pin_code(546)
+        incorrect_at_2 = self.terminal.enter_pin_code(546)
+        incorrect_at_3 = self.terminal.enter_pin_code(546)
+        self.assertEqual(self.terminal.attempts, 0)
         self.assertTrue(self.terminal.user_can_get_money)
+
+    def test_user_cant_top_up_0(self):
+        #Test should be down
+        my_money = self.terminal.top_up_money(0)
+        self.assertEqual(my_money, 5000)
+
+    def test_user_cant_top_up_negative(self):
+        my_money = self.terminal.top_up_money(-1)
+        self.assertEqual(my_money, 5000)
+        #to do: symbols, symbols+numbers
